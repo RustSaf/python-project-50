7d4862a97c47b9a1b4e1a468084b79aa38653279
Rust <afarg1710@gmail.com>
2024-08-22 20:46:13 +0500

    Final commit 8 of 2 project

--- gendiff/format/data_to_str.py
+++ gendiff/format/data_to_str.py
@@ -2,4 +2,5 @@
 def data_to_str(val, quotes=0):
     return 'null' if val is None else (
         str(val).lower() if isinstance(val, bool) else (
-            f"'{str(val)}'" if quotes == 1 else str(val)))
+            str(val) if isinstance(val, int) else (
+                f"'{str(val)}'" if quotes == 1 else str(val))))
--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -11,12 +11,12 @@ def make_stylish(data, i=0):
                 result += '    '*i + '  + ' + key + ': ' + data_to_str(
                     make_stylish(data[key].get('added'), i+1) if isinstance(
                         data[key].get('added'), dict
-                    ) else data[key].get('added')) + '\n'
+                            ) else data[key].get('added')) + '\n'
             elif 'deleted' in data[key]:
                 result += '    '*i + '  - ' + key + ': ' + data_to_str(
                     make_stylish(data[key].get('deleted'), i+1) if isinstance(
                         data[key].get('deleted'), dict
-                    ) else data[key].get('deleted')) + '\n'
+                            ) else data[key].get('deleted')) + '\n'
             elif 'unchanged' in data[key]:
                 result += '    '*i + '    ' + key + ': ' + data_to_str(
                     make_stylish(data[key].get(
@@ -27,11 +27,11 @@ def make_stylish(data, i=0):
                 result += '    '*i + '  - ' + key + ': ' + data_to_str(
                     make_stylish(data[key].get('old'), i+1) if isinstance(
                         data[key].get('old'), dict
-                    ) else data[key].get('old')) + '\n'
+                            ) else data[key].get('old')) + '\n'
                 result += '    '*i + '  + ' + key + ': ' + data_to_str(
                     make_stylish(data[key].get('new'), i+1) if isinstance(
                         data[key].get('new'), dict
-                    ) else data[key].get('new')) + '\n'
+                            ) else data[key].get('new')) + '\n'
             else:
                 result += '    '*(i+1) + key + ': ' + data_to_str(
                     make_stylish(data[key], i+1)) + '\n'
