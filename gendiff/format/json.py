3a131acd9e83e56b32b51cf69237306cb515a60c
Rust <afarg1710@gmail.com>
2024-08-25 21:27:07 +0500

    Last commit 2 of 2 project

--- gendiff/format/json.py
+++ gendiff/format/json.py
@@ -4,7 +4,8 @@ import json
 
 def make_json(data):
     with open('tests/fixtures/file_result_tmp.json', 'w') as f:
-        json.dump(data, f, sort_keys=True, indent=2)
+        # json.dump(data, f, sort_keys=True, indent=2)
+        json.dump(data, f, sort_keys=True)
 
     with open('tests/fixtures/file_result_tmp.json') as f:
         result = f.read()
