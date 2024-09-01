c42fde8a0bc50455d3c6c294b217f45cbcab5cfb
Rust <afarg1710@gmail.com>
2024-09-01 22:52:35 +0500

    4 commit after refactoring of 2 project

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -11,35 +11,35 @@ def make_stylish(data, i=0):
                 result += (
                     f"{'    ' * i}  + {key}: "
                     f"""{data_to_str(make_stylish(data[key].get('added'),
-                        i + 1) if isinstance(data[key].get('added'), dict)
-                        else data[key].get('added'))}\n"""
+                         i + 1) if isinstance(data[key].get('added'), dict)
+                         else data[key].get('added'))}\n"""
                 )
             elif 'deleted' in data[key]:
                 result += (
                     f"{'    ' * i}  - {key}: "
                     f"""{data_to_str(make_stylish(data[key].get('deleted'),
-                        i + 1) if isinstance(data[key].get('deleted'), dict)
-                        else data[key].get('deleted'))}\n"""
+                         i + 1) if isinstance(data[key].get('deleted'), dict)
+                         else data[key].get('deleted'))}\n"""
                 )
             elif 'unchanged' in data[key]:
                 result += (
                     f"{'    ' * i}    {key}: "
                     f"""{data_to_str(make_stylish(data[key].get('unchanged'),
-                        i + 1) if isinstance(data[key].get('unchanged'), dict)
-                        else data[key].get('unchanged'))}\n"""
+                         i + 1) if isinstance(data[key].get('unchanged'), dict)
+                         else data[key].get('unchanged'))}\n"""
                 )
             elif 'old' and 'new' in data[key]:
                 result += (
                     f"{'    ' * i}  - {key}: "
                     f"""{data_to_str(make_stylish(data[key].get('old'), i + 1)
-                        if isinstance(data[key].get('old'), dict)
-                        else data[key].get('old'))}\n"""
+                         if isinstance(data[key].get('old'), dict)
+                         else data[key].get('old'))}\n"""
                 )
                 result += (
                     f"{'    ' * i}  + {key}: "
                     f"""{data_to_str(make_stylish(data[key].get('new'), i + 1)
-                        if isinstance(data[key].get('new'), dict)
-                        else data[key].get('new'))}\n"""
+                         if isinstance(data[key].get('new'), dict)
+                         else data[key].get('new'))}\n"""
                 )
             else:
                 result += (
