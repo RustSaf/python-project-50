ad3064dc32c205d63ef22bb23a31a0e8c5858a57
Rust <afarg1710@gmail.com>
2024-09-11 06:56:14 +0500

    Commit with new branch

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -2,51 +2,76 @@
 from gendiff.format.data_to_str import data_to_str
 
 
-def make_stylish(data, i=0):
+def make_stylish(data, depth=0):
+    result = []
     keys = sorted(data.keys())
-    result = ""
     INDENT = '    '
     for key in keys:
-        if isinstance(data[key], dict):
-            if data[key].get('key') == 'added':
-                result += (
-                    f"{INDENT * i}  + {key}: "
-                    f"""{data_to_str(make_stylish(data[key].get('value'),
-                         i + 1) if isinstance(data[key].get('value'), dict)
-                         else data[key].get('value'))}\n"""
-                    )
-            elif data[key].get('key') == 'deleted':
-                result += (
-                    f"{INDENT * i}  - {key}: "
-                    f"""{data_to_str(make_stylish(data[key].get('value'),
-                         i + 1) if isinstance(data[key].get('value'), dict)
-                         else data[key].get('value'))}\n"""
-                )
-            elif data[key].get('key') == 'unchanged':
-                result += (
-                    f"{INDENT * i}    {key}: "
-                    f"""{data_to_str(make_stylish(data[key].get('value'),
-                         i + 1) if isinstance(data[key].get('value'), dict)
-                         else data[key].get('value'))}\n"""
-                )
-            elif data[key].get('key') == 'changed':
-                result += (
-                    f"{INDENT * i}  - {key}: "
-                    f"""{data_to_str(make_stylish(data[key].get('value1'),
-                         i + 1) if isinstance(data[key].get('value1'), dict)
-                         else data[key].get('value1'))}\n"""
-                )
-                result += (
-                    f"{INDENT * i}  + {key}: "
-                    f"""{data_to_str(make_stylish(data[key].get('value2'),
-                         i + 1) if isinstance(data[key].get('value2'), dict)
-                         else data[key].get('value2'))}\n"""
-                )
-            else:
-                result += (
-                    f"{INDENT * (i + 1)}{key}: "
-                    f"{data_to_str(make_stylish(data[key], i + 1))}\n"
-                )
+        if data[key].get('type') == 'added':
+            result.append(f"{INDENT*depth}+ {data[key].get('key')}: "
+                          f"{data_to_str(data[key].get('value'))}"
+            )
+        elif data[key].get('type') == 'deleted':
+            result.append(f"{INDENT*depth}- {data[key].get('key')}: "
+                          f"{data_to_str(data[key].get('value'))}"
+            )
+        elif data[key].get('type') == 'children':
+            result.append(f"{INDENT*depth} {data[key].get('key')}: "
+                          f"{data_to_str(data[key].get('value'))}"
+            )
+        elif data[key].get('type') == 'nested':
+            lines = map(lambda child: make_stylish(child, depth + 1), data[key].get('value'))
+            sub_result = '\n'.join(lines)
+            result.append( f"{INDENT*depth}  {data[key].get('key')}: {{\n{sub_result}\n{INDENT}  }}")
         else:
-            result += f"{INDENT * (i + 1)}{key}: {data_to_str(data[key])}\n"
-    return f"{{\n{result}{INDENT * i}}}"
+            pass
+    return result
+
+# def make_stylish(data, i=0):
+#     keys = sorted(data.keys())
+#     result = ""
+#     INDENT = '    '
+#     for key in keys:
+#         if isinstance(data[key], dict):
+#             if data[key].get('key') == 'added':
+#                 result += (
+#                     f"{INDENT * i}  + {key}: "
+#                     f"""{data_to_str(make_stylish(data[key].get('value'),
+#                          i + 1) if isinstance(data[key].get('value'), dict)
+#                          else data[key].get('value'))}\n"""
+#                     )
+#             elif data[key].get('key') == 'deleted':
+#                 result += (
+#                     f"{INDENT * i}  - {key}: "
+#                     f"""{data_to_str(make_stylish(data[key].get('value'),
+#                          i + 1) if isinstance(data[key].get('value'), dict)
+#                          else data[key].get('value'))}\n"""
+#                 )
+#             elif data[key].get('key') == 'unchanged':
+#                 result += (
+#                     f"{INDENT * i}    {key}: "
+#                     f"""{data_to_str(make_stylish(data[key].get('value'),
+#                          i + 1) if isinstance(data[key].get('value'), dict)
+#                          else data[key].get('value'))}\n"""
+#                 )
+#             elif data[key].get('key') == 'changed':
+#                 result += (
+#                     f"{INDENT * i}  - {key}: "
+#                     f"""{data_to_str(make_stylish(data[key].get('value1'),
+#                          i + 1) if isinstance(data[key].get('value1'), dict)
+#                          else data[key].get('value1'))}\n"""
+#                 )
+#                 result += (
+#                     f"{INDENT * i}  + {key}: "
+#                     f"""{data_to_str(make_stylish(data[key].get('value2'),
+#                         i + 1) if isinstance(data[key].get('value2'), dict)
+#                          else data[key].get('value2'))}\n"""
+#                 )
+#             else:
+#                 result += (
+#                     f"{INDENT * (i + 1)}{key}: "
+#                     f"{data_to_str(make_stylish(data[key], i + 1))}\n"
+#                 )
+#         else:
+#             result += f"{INDENT * (i + 1)}{key}: {data_to_str(data[key])}\n"
+#     return f"{{\n{result}{INDENT * i}}}"
--- gendiff/generator.py
+++ gendiff/generator.py
@@ -4,15 +4,41 @@ def build_tree(data1, data2):
     data = {}
     for key in keys:
         if key not in data1:
-            data[key] = {'key': 'added', 'value': data2[key]}
+            data[key] = {'type': 'added', 'key': key, 'value': build_tree(
+                data2[key], data2[key]) if isinstance(
+                    data2[key], dict) else data2[key]}
         elif key not in data2:
-            data[key] = {'key': 'deleted', 'value': data1[key]}
-        elif data1[key] == data2[key]:
-            data[key] = {'key': 'unchanged', 'value': data1[key]}
-        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
-            data[key] = build_tree(data1[key], data2[key])
-        else:
+            data[key] = {'type': 'deleted', 'key': key, 'value': build_tree(
+                data1[key], data1[key]) if isinstance(
+                    data1[key], dict) else data1[key]}
+        # elif data1[key] == data2[key]:
+        #     data[key] = {'type': 'unchanged', 'key': key, 'value': build_tree(
+        #         data1[key], data2[key]) if isinstance(
+        #             data1[key], dict) else data1[key]}
+        elif isinstance(data1[key], dict):
+            data[key] = {'type': 'nested', 'key': key, 'value': build_tree(
+                data1[key], data1[key])}
+        elif isinstance(data2[key], dict):
+            data[key] = {'type': 'nested', 'key': key, 'value': build_tree(
+                data2[key], data2[key])}
+        # elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+        #     data[key] = {'type': 'nested', 'key': key, 'value': build_tree(
+        #         data1[key], data2[key])}
+        elif data1[key] != data2[key]:
             data[key] = {
-                'key': 'changed', 'value1': data1[key], 'value2': data2[key]
+                'type': 'changed', 'key': key, 'value1': data1[key], 'value2': data2[key]
             }
+        # elif data1[key] != data2[key]:
+        #     data[key] = {'type': 'changed', 'key': key, 'value1': build_tree(
+        #         data1[key], data1[key]) if isinstance(
+        #             data1[key], dict) else data1[key], 'value2': build_tree(
+        #                 data2[key], data2[key]) if isinstance(
+        #                     data2[key], dict) else data2[key]}
+        # elif isinstance(data1[key], dict) or isinstance(data2[key], dict):
+        #     data[key] = {'type': 'nested', 'key': key, 'value': build_tree(
+        #         data1[key], data1[key]) if isinstance(
+        #             data1[key], dict) else build_tree(
+        #                 data2[key], data2[key])}
+        # else:
+        #    data[key] = {'type': 'children', 'key': key, 'value': data1[key]}
     return data
