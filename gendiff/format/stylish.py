aa3298047c32f01465f18fde43598848fc2ed7af
Rust <afarg1710@gmail.com>
2024-10-02 00:10:17 +0500

    Its ALL ALL

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -12,15 +12,15 @@ def to_str(data, depth=0):
         keys = sorted(data.keys())
         for key in keys:
             if data[key].get('type') == 'children':
-                result.append(f"{INDENT*(depth + 1)}{key}: "
+                result.append(f"{INDENT * (depth + 1)}{key}: "
                               f"{data_to_str(data[key].get('value'))}")
             else:
-                result.append(f"{INDENT*(depth + 1)}{key}: "
+                result.append(f"{INDENT * (depth + 1)}{key}: "
                               f"{to_str(data[key].get('value'), depth + 1)}")
     else:
         return data_to_str(data)
     result_line = '\n'.join(result)
-    return f"{{\n{result_line}\n{INDENT*depth}}}"
+    return f"{{\n{result_line}\n{INDENT * depth}}}"
 
 
 def make_stylish(data, depth=0):
@@ -29,21 +29,21 @@ def make_stylish(data, depth=0):
     keys = sorted(data.keys())
     for key in keys:
         if data[key].get('type') == 'added':
-            result.append(f"{INDENT*depth}  + {key}: "
+            result.append(f"{INDENT * depth}  + {key}: "
                           f"{to_str(data[key].get('value'), depth + 1)}")
         elif data[key].get('type') == 'deleted':
-            result.append(f"{INDENT*depth}  - {key}: "
+            result.append(f"{INDENT * depth}  - {key}: "
                           f"{to_str(data[key].get('value'), depth + 1)}")
         elif data[key].get('type') == 'changed':
-            result.append(f"{INDENT*depth}  - {key}: "
+            result.append(f"{INDENT * depth}  - {key}: "
                           f"{to_str(data[key].get('value1'), depth + 1)}")
-            result.append(f"{INDENT*depth}  + {key}: "
+            result.append(f"{INDENT * depth}  + {key}: "
                           f"{to_str(data[key].get('value2'), depth + 1)}")
         elif data[key].get('type') == 'children':
-            result.append(f"{INDENT*depth}    {key}: "
+            result.append(f"{INDENT * depth}    {key}: "
                           f"{to_str(data[key].get('value'))}")
         else:
-            result.append(f"{INDENT*(depth + 1)}{key}: "
+            result.append(f"{INDENT * (depth + 1)}{key}: "
                           f"{make_stylish(data[key].get('value'), depth + 1)}")
     result_line = '\n'.join(result)
-    return f"{{\n{result_line}\n{INDENT*depth}}}"
+    return f"{{\n{result_line}\n{INDENT * depth}}}"
